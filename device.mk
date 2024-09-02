094621fba759703bd2c34ffec7c02f8fab6877a3
Alexander Martinz <amartinz@shiftphones.com>
2024-09-02 12:00:42 +0200

    otter: build otter specific fingerprint service
    Change-Id: I4a25e52cd82178c0a9d9a0e57e4e9295809653ce
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- device.mk
+++ device.mk
@@ -148,7 +148,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.3-service.otter
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
