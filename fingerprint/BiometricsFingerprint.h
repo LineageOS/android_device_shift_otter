cc6df252464ccf37c4aab64cc0b0625b8986c548
Michael Bestas <mkbestas@lineageos.org>
2024-09-18 05:08:38 +0300

    otter: fingerprint: Import fingerprint.h
    Change-Id: I7b0cc62fdb4d04ae4966a030964f0bee12182314

--- fingerprint/BiometricsFingerprint.h
+++ fingerprint/BiometricsFingerprint.h
@@ -20,7 +20,7 @@
 #include <log/log.h>
 #include <android/log.h>
 #include <hardware/hardware.h>
-#include <hardware/fingerprint.h>
+#include "fingerprint.h"
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
 #include <android/hardware/biometrics/fingerprint/2.3/IBiometricsFingerprint.h>
