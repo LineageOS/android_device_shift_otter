b40249b8c7d02a9a86b0df17f0db9d2a2d144d39
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 09:03:01 +0300

    otter: Drop no longer used TARGET_KERNEL_NO_GCC
    See: I419ffd8c4f055ca29a6554983ed44aaaab54a666
    Change-Id: I9ce189a991bddd4ec9e42f6853fe5fcd12b51038

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,7 +90,6 @@ BOARD_KERNEL_CMDLINE := \
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig vendor/otter.config
-TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/shift/qcm6490
 
 # Kernel modules
